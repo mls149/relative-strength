e374564537778d7e634af4cf047fe555917df74a
mls149 <50207565+mls149@users.noreply.github.com>
2024-11-04 23:32:21 +0800

    Force use of Yahoo

--- relative-strength.py
+++ relative-strength.py
@@ -3,13 +3,14 @@ import rs_ranking
 import sys
 
 def main():
-   skipEnter = None if len(sys.argv) <= 1 else sys.argv[1]
-   forceTDA = None if len(sys.argv) <= 2 else sys.argv[2]
-   api_key = None if len(sys.argv) <= 3 else sys.argv[3]
-   if api_key:
-      rs_data.main(forceTDA=="true", api_key)
-   else:
-      rs_data.main(forceTDA=="true")
+   skipEnter = "true" #None if len(sys.argv) <= 1 else sys.argv[1]
+   forceTDA = "false" #None if len(sys.argv) <= 2 else sys.argv[2]
+#   api_key = None if len(sys.argv) <= 3 else sys.argv[3]
+#   if api_key:
+#      rs_data.main(forceTDA=="true", api_key)
+#   else:
+#      rs_data.main(forceTDA=="true")
+   rs_data.main(forceTDA=="true")
    rs_ranking.main(skipEnter=="true")
 
 if __name__ == "__main__":
